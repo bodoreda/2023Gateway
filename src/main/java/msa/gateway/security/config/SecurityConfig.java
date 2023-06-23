a4b8d1ec3e3cf98a09571076ee54f54040d84a57
bodoreda <bodoreda@gmail.com>
2023-06-23 17:08:57 +0900

    [gateway] Spring Security / JWT 의존성 추가, 디렉토리 init

---
+++ src/main/java/msa/gateway/security/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package msa.gateway.security.config;
+
+/**
+ * packageName : msa.gateway.security.config
+ * fileName : SecurityConfig
+ * author : BH
+ * date : 2023-06-23
+ * description :
+ * ================================================
+ * DATE                AUTHOR              NOTE
+ * ================================================
+ * 2023-06-23       SecurityConfig       최초 생성
+ */
+public class SecurityConfig {
+}
---
+++ src/main/java/msa/gateway/security/jwt/JwtTokenProvider.java
@@ -0,0 +1,15 @@
+package msa.gateway.security.jwt;
+
+/**
+ * packageName : msa.gateway.security.jwt
+ * fileName : JwtTokenProvider
+ * author : BH
+ * date : 2023-06-23
+ * description :
+ * ================================================
+ * DATE                AUTHOR              NOTE
+ * ================================================
+ * 2023-06-23       JwtTokenProvider       최초 생성
+ */
+public class JwtTokenProvider {
+}
