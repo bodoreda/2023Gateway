e9a8a38f80833ba69b682d55532b589a3dbfccae
bodoreda <bodoreda@gmail.com>
2023-07-13 10:52:13 +0900

    [Gateway, Member] 로그인 시 accessToken 생성 후 헤더 추가, 검증 개발 완료. refreshToken 검증 후 accessToken 재발급 로직 개발 필요

--- src/main/java/msa/gateway/common/filter/GlobalFilter.java
+++ src/main/java/msa/gateway/common/filter/GlobalFilter.java
@@ -1,46 +1,75 @@
 package msa.gateway.common.filter;
 
+import lombok.Data;
 import lombok.extern.log4j.Log4j2;
+import msa.gateway.common.jwt.JwtTokenProvider;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
-import reactor.core.publisher.Mono;
 
 /**
  * packageName : msa.gateway.common.filter
- * fileName : GlobalFilter
+ * fileName : JwtFilter
  * author : BH
  * date : 2023-07-11
  * description :
  * ================================================
  * DATE                AUTHOR              NOTE
  * ================================================
- * 2023-07-11       GlobalFilter       최초 생성
+ * 2023-07-11       JwtFilter       최초 생성
  */
 @Component
 @Log4j2
-public class GlobalFilter extends AbstractGatewayFilterFactory {
+public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
+    private final JwtTokenProvider jwtTokenProvider;
+    public GlobalFilter(JwtTokenProvider jwtTokenProvider) {
+        super(Config.class);
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
     @Override
-    public GatewayFilter apply(Object config) {
-        // Global Pre Filter
-        return (exchange, chain) -> {
+    public GatewayFilter apply(Config config) {
+        // Custom Pre Filter
+        return ((exchange, chain) -> {
+            log.info("Global filter baseMessage : {}", config.getBaseMessage());
+
             ServerHttpRequest request = exchange.getRequest();
             ServerHttpResponse response = exchange.getResponse();
 
-            log.info("Global Filter baseMessage : {}", config.getBaseMessage());
+            String requestPath = request.getPath().toString();
+            String requestHeader = request.getHeaders().getFirst("Authorization");
 
-            if(config.isPreLogger()){
-                log.info("Global Filter Start : request id -> {}", request.getId());
+            // 예외 처리: 로그인 요청은 필터를 거치지 않고 직접 백엔드 서비스로 전달
+            if ("/v1/member/login".equals(requestPath)) {
+                log.info("로그인 예외 처리");
+                return chain.filter(exchange);
             }
 
-            // Global Post Filter
-            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-                if(config.isPostLogger()) {
-                    log.info("Global Filter End : response code -> {}", response.getStatusCode());
+            // Authorization(accessToken) 검증
+            if (requestHeader != null && requestHeader.startsWith("Bearer ")) {
+                log.info("accessToken 검증");
+                String accessToken = requestHeader.substring(7);
+
+                if (jwtTokenProvider.validateToken(accessToken)) {
+                    return chain.filter(exchange);
                 }
-            }));
-        };
+            }
+
+            // 검증 실패하거나 Authorization 헤더가 없는 경우, 401 Unauthorized 에러를 반환합니다.
+            log.info("401 에러 반환");
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            return response.setComplete();
+        });
+    }
+
+    @Data
+    public static class Config{
+        // Put the configuration properties... application.yml에 작성
+        private String baseMessage;
+        private boolean preLogger;
+        private boolean postLogger;
     }
 }
